1d6ef249192c4d7b5e75bf468a2bd7bc33be3c5b
XiaotaoChen <chenxiaotao1234@gmail.com>
2018-08-06 19:31:46 +0800

    remove unuse code

--- example/image-classification/symbols/resnext.py
+++ example/image-classification/symbols/resnext.py
@@ -183,15 +183,6 @@ def resnext_stage(num_classes, num_group, image_shape, bottle_neck=True, bn_mom=
             data = mx.sym.Cast(data=data, dtype=np.float16)
     data = mx.sym.BatchNorm(data=data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
     (nchannel, height, width) = image_shape
-    # if height <= 32:            # such as cifar10
-    #     body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(3, 3), stride=(1,1), pad=(1, 1),
-    #                               no_bias=True, name="conv0", workspace=workspace)
-    # else:                       # often expected to be 224 such as imagenet
-    #     body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(7, 7), stride=(2,2), pad=(3, 3),
-    #                               no_bias=True, name="conv0", workspace=workspace)
-    #     body = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=2e-5, momentum=bn_mom, name='bn0')
-    #     body = mx.sym.Activation(data=body, act_type='relu', name='relu0')
-    #     body = mx.sym.Pooling(data=body, kernel=(3, 3), stride=(2,2), pad=(1,1), pool_type='max')
     if stage == 'stage1':
         body = mx.sym.Convolution(data=data, num_filter=64, kernel=(7, 7), stride=(2, 2), pad=(3, 3),
                                   no_bias=True, name="conv0", workspace=workspace)
@@ -247,15 +238,6 @@ def resnext_stage(num_classes, num_group, image_shape, bottle_neck=True, bn_mom=
             body = residual_unit(body, 2048, (1, 1), True, name='stage4_unit%d' % (j + 2),
                                  bottle_neck=bottle_neck, num_group=num_group, bn_mom=bn_mom,
                                  workspace=workspace, memonger=memonger)
-
-
-    # for i in range(num_stages):
-    #     body = residual_unit(body, filter_list[i+1], (1 if i==0 else 2, 1 if i==0 else 2), False,
-    #                          name='stage%d_unit%d' % (i + 1, 1), bottle_neck=bottle_neck, num_group=num_group,
-    #                          bn_mom=bn_mom, workspace=workspace, memonger=memonger)
-    #     for j in range(units[i]-1):
-    #         body = residual_unit(body, filter_list[i+1], (1,1), True, name='stage%d_unit%d' % (i + 1, j + 2),
-    #                              bottle_neck=bottle_neck, num_group=num_group, bn_mom=bn_mom, workspace=workspace, memonger=memonger)
 
     pool1 = mx.sym.Pooling(data=body, global_pool=True, kernel=(7, 7), pool_type='avg', name='pool1')
     flat = mx.sym.Flatten(data=pool1)
